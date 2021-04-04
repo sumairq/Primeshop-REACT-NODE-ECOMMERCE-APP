69c1586b34c797be6783fcccf0a6edb0d6e7d4c2
Sumair <sumairqaisar992@gmail.com>
2021-04-04 23:52:11 +0500

    New deployment

---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'Welcome To Proshop',
+  keywords: '',
+}
+
+export default Meta
